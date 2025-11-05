e4719207e21a8856916805d153db77bce8c3453e
Esakki <esakkiraj@webstix.com>
2025-11-05 11:14:31 +0530

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom"; // <-- Use HashRouter
+import { Routes, Route } from "react-router-dom"; 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
@@ -10,8 +10,6 @@ import Team from "./pages/Team";
 import Culture from "./pages/Culture";
 import Careers from "./pages/Careers";
 import GalleryPreviewPage from "./pages/GalleryPreviewPage";
-import EventGalleryPage from "./pages/EventGalleryPage";
-import EventPreviewPage from "./pages/EventPreviewPage";
 import Contact from "./pages/Contact";
 import ApplyJob from "./pages/ApplyJob";
 import AdminLogin from "./pages/admin/AdminLogin";
@@ -30,7 +28,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <>
         <Routes>
           {/* Public Pages */}
           <Route path="/" element={<Index />} />
@@ -44,8 +42,6 @@ const App = () => (
           
           {/* Gallery Routes */}
           <Route path="/gallery-main" element={<GalleryPreviewPage />} />
-          <Route path="/gallery/:categoryId" element={<EventGalleryPage />} />
-          <Route path="/gallery/event/:eventId" element={<EventPreviewPage />} />
 
           {/* Admin & Auth */}
           <Route path="/admin/login" element={<AdminLogin />} />
@@ -58,7 +54,7 @@ const App = () => (
           {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </>
     </TooltipProvider>
   </QueryClientProvider>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/wdpl.in">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
