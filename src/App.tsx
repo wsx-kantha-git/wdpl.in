3e66e93290f706c0053f18aba66b85281cf43786
Esakki <esakkiraj@webstix.com>
2025-11-05 11:37:54 +0530

    Fix GitHub Pages routing Temporary

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom"; 
+import { HashRouter,Routes, Route } from "react-router-dom"; 
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
@@ -28,7 +28,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <>
+      <HashRouter>
         <Routes>
           {/* Public Pages */}
           <Route path="/" element={<Index />} />
@@ -54,7 +54,7 @@ const App = () => (
           {/* 404 */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
