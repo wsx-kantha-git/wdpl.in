e63c18c390fe5f9f0c4c7628e3d9d5ec9e372144
Esakki <esakkiraj@webstix.com>
2025-11-21 12:59:40 +0530

    Deploy Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // add this line for GitHub Pages
+  base: "./", // add this line for GitHub Pages
   server: {
     host: "::",
     port: 8080,
